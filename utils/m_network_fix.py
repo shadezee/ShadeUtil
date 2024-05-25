086071e3bbf4e631468868ea34409e3ff3ac230d
shadezee <shadedev25@gmail.com>
2024-05-25 22:43:44 +0530

    update app

--- utils/m_network_fix.py
+++ utils/m_network_fix.py
@@ -1,10 +1,8 @@
 from subprocess import run
-from utils.support.m_run_as_admin import *
 
 def network_fix():
     disable_interface_command = 'netsh interface set interface Wi-Fi disable'
     enable_interface_command = 'netsh interface set interface Wi-Fi enable'
-
     try:
         run(disable_interface_command, shell=True, check=False)
         run(enable_interface_command, shell=True, check=False)
--- utils/support/m_run_as_admin.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-import sys
-
-def run_as_admin_user():
-    if sys.platform == 'win32':
-        if ctypes.windll.shell32.IsUserAnAdmin() == 0:
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-            sys.exit()
